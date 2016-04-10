33ce6f6544d1e05112c12051d35b1db223bd44b2
RyodoTanaka <k104073r@mail.kyutech.jp>
2016-04-10 14:14:32 +0900

    [add & modify] add filtered data. modify the prog
    modify : delete viewer

--- filtering/voxel_grid/voxel_grid.cpp
+++ filtering/voxel_grid/voxel_grid.cpp
@@ -2,20 +2,43 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/cloud_viewer.h>
 
-int
-main (int argc, char** argv)
+using namespace std;
+
+void viewerOneOff(pcl::visualization::PCLVisualizer& viewer)
+{
+  viewer.setBackgroundColor(0, 0, 0);
+  //pcl::PointXYZ o;
+  //o.x = 1.0;
+  //o.y = 0;
+  //o.z = 0;
+  // viewer.addSphere(o, 0.25, "sphere", 0);
+  cout << "I only run once" << endl;
+}
+
+// void viewerPsycho(pcl::visualization::PCLVisualizer& viewer)
+// {
+//   static unsigned count = 0;
+//   stringstream ss;
+//   ss << "Once per viewer loop" << count++;
+//   viewer.removeShape("text", 0);
+//   viewer.addText(ss.str(), 200, 300, "text", 0);
+// }
+
+
+int main (int argc, char** argv)
 {
   pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2 ());
   pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2 ());
-
+  
   // Fill in the cloud data
   pcl::PCDReader reader;
   // Replace the path below with the path where you saved your file
   reader.read ("../data/table_scene_lms400.pcd", *cloud); // Remember to download the file first!
 
   std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
-       << " data points (" << pcl::getFieldsList (*cloud) << ").";
+			<< " data points (" << pcl::getFieldsList (*cloud) << ").";
 
   // Create the filtering object
   pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
@@ -24,11 +47,11 @@ main (int argc, char** argv)
   sor.filter (*cloud_filtered);
 
   std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
-       << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
+			<< " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
 
   pcl::PCDWriter writer;
-  writer.write ("table_scene_lms400_downsampled.pcd", *cloud_filtered, 
-         Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
+  writer.write ("../data/table_scene_lms400_downsampled.pcd", *cloud_filtered, 
+				Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false); 
 
   return (0);
 }
